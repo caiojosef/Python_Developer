81e86cd4de0fe734f1c29f348a7cb7d1dd45a26b
Caio Josef <102381112+caiojosef@users.noreply.github.com>
2023-02-10 16:47:32 -0300

    Initial commit

---
+++ ConhecendoALinguagemDeProgramaçãoPython/TiposDeDados.py
@@ -0,0 +1,14 @@
+#inteiros
+print(11 + 10)
+#floats
+print(1.5 + 2.30)
+#String
+print("Hello World")
+#boolean
+print(True)
+print(False)
+
+int()
+float()
+str()
+bool()
---
+++ ConhecendoALinguagemDeProgramaçãoPython/VariaveisEConstantes.py
@@ -0,0 +1,7 @@
+age = 29
+name = 'Caio Josef'
+print(f'Meu nome é {name} e eu tenho {age} anos de idade.')
+
+age, name = (29, 'Caio Josef')
+print(f'\nMeu nome é {name} e eu tenho {age} anos de idade.')
+
